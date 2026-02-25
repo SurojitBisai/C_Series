0740b924e62b3e742de4b69266819419f9e9d1c2
Surojit Bisai <surojitbisai8328@gmail.com>
2026-02-25 17:05:42 +0530

    redius

---
+++ area_of_squar_radious.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("Enter the radius");
+    scanf("%f",&radius );
+    printf("Area of radius is %f", 3.14 * radius * radius);
+    return 0;
+}
