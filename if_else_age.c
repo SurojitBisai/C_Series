fa71ed2a89db0dd62a2c9af7ead4aebd3f8566b4
Surojit Bisai <surojitbisai8328@gmail.com>
2026-03-16 19:27:49 +0530

    age

---
+++ if_else_age.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter the age");
+    scanf("%d", &age);
+    if(age >= 18){
+        printf("adult");
+    }
+    else if (age > 13 && age < 18){
+        printf("Teenager");
+
+    }
+    else {
+        printf("Child");
+
+    }
+    return 0;
+}
