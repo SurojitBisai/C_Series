7e284d7ac00951171f35a7739e9fefe537076419
Surojit Bisai <surojitbisai8328@gmail.com>
2026-04-11 15:43:23 +0530

    ternary operater in marks

---
+++ marks_ternary_opera.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int marks;
+    printf("Ennter the marks:");
+    scanf("%d", &marks);
+    marks <= 30 ? printf("Fail\n") : printf("Pass\n");
+
+
+    return 0;
+}
