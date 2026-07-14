cf8f70ecd5c3eae470f3202ff88be3e2b90549d7
Surojit Bisai <surojitbisai8328@gmail.com>
2026-07-15 00:32:54 +0530

    Two Functio Print Hello and Goodbye

---
+++ functiom_print_hello_good.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//function declaration/ prototype
+void printHello();
+void printGoodbye();
+
+int  main(){
+    printHello();//function call
+    printGoodbye();
+    return 0;
+}
+//function definition
+void printHello(){
+    printf("Hello!\n");
+}
+
+void printGoodbye(){
+    printf("Goodbye:)\n");
+}
